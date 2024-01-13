61256821c4b044b2c4603553afe3d4f8a58124fc
Igauk <igauk@ualberta.ca>
2024-01-13 13:43:56 -0700

    Remove unused imports

--- src/service/image_transformer.py
+++ src/service/image_transformer.py
@@ -1,8 +1,5 @@
-import sys
-
 import cv2
 import numpy as np
-from skimage.filters import try_all_threshold
 from skimage.measure import label, regionprops
 from matplotlib import pyplot as plt
 
@@ -57,48 +54,3 @@ def _get_edge_mask(edges, kernel_dim=2, kernel_shape=cv2.MORPH_RECT, iterations=
         closing = np.pad(closing, ((0, iterations), (0, iterations)), constant_values=0)
     edge_mask = closing == 255
     return edge_mask
-
-def remove_segments(binary_image, min_area=None, max_area=None, max_dimension=None, background=0):
-    labeled_image = label(binary_image, connectivity=2, background=background)
-    regions = regionprops(labeled_image)
-    minimum = min_area if min_area else 0
-    maximum = max_area if max_area else sys.maxsize
-    max_dim = max_dimension if max_dimension else sys.maxsize
-    regions_to_remove = []
-    for region in regions:
-        if region.area < minimum or region.area > maximum:
-            regions_to_remove.append(region)
-        elif region.bbox[3] - region.bbox[1] > max_dim or region.bbox[2] - region.bbox[0] > max_dim:
-            regions_to_remove.append(region)
-    for region in regions_to_remove:
-        binary_image[labeled_image == region.label] = background
-    return binary_image
-
-
-def keep_white_pixels(image: np.ndarray, luminance_threshold=120, grayscale_lenience=100, background=0) -> np.ndarray:
-    """
-    Transforms an image by maintaining grayscale colors
-
-    :param luminance_threshold: Luminance cutoff
-    :param image: RGB colorspace image
-    :param grayscale_lenience
-    :param background determines the background color of the image, all non-grayscale values
-    will be converted to background
-    :return Image with only grayscale values maintained
-    """
-    luminance_threshold_mask = np.full(shape=3, fill_value=luminance_threshold)
-    luminance_mask = np.all(image >= luminance_threshold_mask, axis=-1)
-    maximum_luminance_channel = np.max(image, axis=-1)
-    minimum_luminance_channel = np.min(image, axis=-1)
-    difference = maximum_luminance_channel - minimum_luminance_channel
-    grayscale_mask = np.where(difference <= grayscale_lenience, True, False)
-    mask = luminance_mask & grayscale_mask
-    processed_image = image.copy()
-    processed_image[~mask] = background
-    processed_image[mask] = 255
-    return processed_image
-
-
-def try_all_binarizations(img):
-    try_all_threshold(img, verbose=False)
-    plt.show()
